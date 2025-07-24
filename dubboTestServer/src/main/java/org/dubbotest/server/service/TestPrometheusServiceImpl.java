f35976885bc4e0829c51f6623f3eecf3df799714
bee-eater <minim4@bee-eater-mhyMac-mini.local>
2025-07-24 19:36:28 +0800

    update

--- dubboTestServer/src/main/java/org/dubbotest/server/service/TestPrometheusServiceImpl.java
+++ dubboTestServer/src/main/java/org/dubbotest/server/service/TestPrometheusServiceImpl.java
@@ -39,8 +39,10 @@ public class TestPrometheusServiceImpl {
         });
     }
 
+    // 注解的方式没有办法传入函数参数，传入的实际是一个字符串 "#{conf.name}"
+    // 使用参数中的tag，还是需要写Timer的方式来实现，如上面testMeter例子所示
     @Counted(value = "reco.api.annotation", description = "testCount")
-    @Timed(value = "reco.api.annotation", percentiles = {0.5, 0.95, 0.99})
+    @Timed(value = "reco.api.annotation", percentiles = {0.5, 0.95, 0.99}, extraTags = {"feature_name", "#{conf.name}"})
     public void testMeterAnnotation(FeatureConf conf) {
 
     }
